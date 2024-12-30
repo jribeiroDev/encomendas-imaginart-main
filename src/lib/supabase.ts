25661df66319a9694801bbc38650ba3011b41f52
João Ribeiro <pedroribeiro15@gmail.com>
2024-12-30 12:53:25 +0000

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,9 @@
-import { createClient } from '@supabase/supabase-js';
+//import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fzylihxwrdmiluhnbaey.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6eWxpaHh3cmRtaWx1aG5iYWV5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNTUzMDI1OCwiZXhwIjoyMDUxMTA2MjU4fQ.kipJ4YbXhbR1GpnF88gUG8MdMjmzkMFvhKC3MGHDWNs'
-export const supabase = createClient(supabaseUrl, supabaseKey);
+//const supabaseUrl = 'https://fzylihxwrdmiluhnbaey.supabase.co';
+//const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6eWxpaHh3cmRtaWx1aG5iYWV5Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTczNTUzMDI1OCwiZXhwIjoyMDUxMTA2MjU4fQ.kipJ4YbXhbR1GpnF88gUG8MdMjmzkMFvhKC3MGHDWNs'
+//export const supabase = createClient(supabaseUrl, supabaseKey);
+
+import { supabase } from "@/integrations/supabase/client";
+
+export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY);
