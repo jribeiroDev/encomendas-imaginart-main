16635700bf68444ecabb749a31d2697168533539
João Ribeiro <pedroribeiro15@gmail.com>
2024-12-30 04:01:22 +0000

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://pfbnlouycrkyfrobrpcs.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBmYm5sb3V5Y3JreWZyb2JycGNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU1MjM1MzUsImV4cCI6MjA1MTA5OTUzNX0.WGunSOtrXJjBZx-XOZNho0h9H6ZaQFcQU4_qC83wFzU';
-
+const supabaseUrl = 'https://fzylihxwrdmiluhnbaey.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6eWxpaHh3cmRtaWx1aG5iYWV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU1MzAyNTgsImV4cCI6MjA1MTEwNjI1OH0.gO87oWY3B0FeYStlnVXYvlMqvI6qnVlFtbc6hCtOBWg'
 export const supabase = createClient(supabaseUrl, supabaseKey);
